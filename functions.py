3737c9decc029619c7b1e6097c958398d4679894
Ibad-ur-Rehman <ibadkps@gmail.com>
2023-09-29 21:29:05 +0500

    make image creation feature

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@ import os
 import openai
 import streamlit as st
 import json
+import requests
 
 
 # function for sign up if signed up
@@ -56,20 +57,47 @@ class Chatbot:
     def __init__(self):
         openai.api_key = os.getenv("OPEN AI KEY")
 
-    def get_response(self, user_input, messages):
+    def get_response(self, user_input, messages, no_of_tokens, temp):
         messages.append({"role": "user", "content": user_input})
         chat = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=messages,
             # prompt = user_input,
-            max_tokens=1000,
-            temperature=0.5
+            max_tokens=no_of_tokens,
+            temperature=temp
         )
         response = chat.choices[0].message.content
         # response = ""
         # if user_input == "asdf":
         #     response = "I am fine!\n How are you I am output I am output I am output I am output I am output I am output I am output I am output I am output I am output I am output I am outputv I am output"
-        return response
+        return  response
+    def get_image(self, user_input, size, no_of_images):
+        #     response = openai.Image.create(
+        #         prompt=user_input,
+        #         n=no_of_images,
+        #         size=size
+        #     )
+        # image_url = response['data'][0]['url']
+        # image_urls = [result['url'] for result in response['data']]
+        # image_urls = {"0":"https://oaidalleapiprodscus.blob.core.windows.net/private/org-7yMSDDPots0mp25LaesEBh5p/user-eJVeNouBg5vAW280BHorayK8/img-KulIRbr2KRdKWK0UUyo2Uc2q.png?st=2023-09-27T15%3A34%3A31Z&se=2023-09-27T17%3A34%3A31Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-09-26T23%3A43%3A09Z&ske=2023-09-27T23%3A43%3A09Z&sks=b&skv=2021-08-06&sig=wikUgj1ZXq9xROJo2uG8knQoi8ycuFE07P4y53zVL3E%3D"}
+        image_urls = [
+            "https://img.freepik.com/free-photo/luxurious-car-parked-highway-with-illuminated-headlight-sunset_181624-60607.jpg",
+            "https://img.freepik.com/premium-photo/bangkok-thailand-08082022-lamborghini-luxury-super-car-fast-sports-premium-lighting-background-3d-illustration_67092-1599.jpg"]
+        if user_input == "asdf":
+            st.write(image_urls)
+            return image_urls
+
+    def edit_image(self, org_image, masked_img, user_input, size, no_of_images):
+        response = openai.Image.create_edit(
+            image=open(org_image, "rb"),
+            mask=open(masked_img, "rb"),
+            prompt=user_input,
+            n=no_of_images,
+            size=size
+        )
+        image_urls = [result['url'] for result in response['data']]
+        st.write(image_urls)
+        return image_urls
 
 
 def typewriter_text(text):
@@ -109,6 +137,50 @@ def typewriter_text(text):
 def make_json_file(dataframe, filename):
     with open(filename, 'w') as json_file:
         json.dump(dataframe, json_file, indent=4)
+
+
+def text_area_img():
+    st.markdown(f"""
+            <style>
+            .stTextArea{{
+                    position: fixed;
+                    bottom: 0;
+                    z-index: 3;
+                    line-height: 9.6;
+                    padding-bottom: 9rem;
+                    caret-color: rgb(250, 250, 250);
+                    color: rgb(250, 250, 250);
+                    border:0px solid white;
+                    border-right: 2px solid #000
+                    padding: 0px;
+                    max-height: 100px;
+                    }}
+                </style>
+            """, unsafe_allow_html=True)
+
+    user_input = st.text_area("", placeholder="Enter more details for better image")
+    return user_input
+
+
+def download_image(url, save_path):
+    try:
+        # Send an HTTP request to the URL to get the image data
+        response = requests.get(url)
+
+        # Check if the request was successful (HTTP status code 200)
+        if response.status_code == 200:
+            # Save the image data to a file in binary mode
+            with open(save_path, 'wb') as file:
+                file.write(response.content)
+            with open(save_path, 'rb') as file:
+                myfile = file.read()
+            st.image(myfile)
+            st.info("Image downloaded successfully.")
+            return myfile
+        else:
+            st.warning("Failed to download image because of network issue")
+    except Exception as e:
+        st.info("Error:" + str(e))
 
 
 if __name__ == "__main__":
---
+++ pages/Image Generator.py
@@ -0,0 +1,242 @@
+import streamlit as st
+import functions
+from io import BytesIO
+from PIL import Image
+import time
+import os
+import glob
+
+
+# finding current time and dat
+current_time = time.localtime()
+day = time.strftime("%d", current_time)
+month = time.strftime("%B", current_time)
+year = time.strftime("%Y", current_time)
+date = f"{day}-{month}-{year}"
+
+# model options to be showed on sidebar
+options = ["Generate new images", "History"]
+
+selected_option = st.sidebar.radio("", options)
+i = 0
+if selected_option == "Generate new images":
+    selection_image_type = ["Create an Image", "Edit an Image", "Make variations of Image"]
+    selection = st.sidebar.selectbox("Selection Menu", selection_image_type)
+
+    if selection == "Create an Image":
+        directory_name = f"files\\images\\Creation\\{date}"
+        if not os.path.exists(directory_name):
+            os.makedirs(directory_name)
+            i = 0
+        elif os.path.exists(directory_name):
+            images = (glob.glob(f"{directory_name}/*.png"))
+            i = len(images)
+        st.markdown(f"<h1 style='text-align: center;'>Create a New Image</h1>", unsafe_allow_html=True)
+        col1, col2 = st.columns(2)
+        with col1:
+            selection_image_size = ["256x256", "512x512", "1024x1024"]
+            selection_size = st.sidebar.radio("Select Size", selection_image_size)
+
+            # size selection menu
+            if selection_size == "256x256":
+                size = "256x256"
+            elif selection_size == "512x512":
+                size = "512x512"
+            elif selection_size == "1024x1024":
+                size = "1024x1024"
+            no_of_images = st.sidebar.number_input("How many image variations you want", min_value=1, max_value=10, value=1)
+
+        user_input = functions.text_area_img()
+        # making an instance of chatbot class
+        image_bot = functions.Chatbot()
+
+        st.image(images)
+
+        Generate_button = st.sidebar.button("Generate Response")
+        if Generate_button:
+            if user_input == "":
+                st.warning("Please Enter some Prompt")
+            else:
+                image_bot_response = image_bot.get_image(user_input, size, no_of_images)
+                for j, image_response_link in enumerate(image_bot_response):
+
+                    img_name = f"{date}"
+                    def save_image():
+                        st.info("Saving Image to disk")
+                        save_path = f"{directory_name}\\{img_name}-{i}.png"
+                        functions.download_image(image_response_link, save_path)
+
+                    save_image()
+                    i += 1
+
+
+    # when Edit image option selected
+    elif selection == "Edit an Image":
+        st.header("Edit an Image")
+        # making two columns 1 for actual image other for masked image
+        directory_name = f"files\\images\\Edited\\{date}"
+        i = 0
+        if not os.path.exists(directory_name):
+            os.makedirs(directory_name)
+            i = 0
+        elif os.path.exists(directory_name):
+            images = (glob.glob(f"{directory_name}/*.png"))
+            i = len(images)
+        col1, col2 = st.columns(2)
+        with col1:
+            st.info("Enter actual image here")
+            user_image_actual = st.file_uploader("", key="file_up1")
+            # resizing the Image
+            width, height = 256, 256
+            # image = user_image.resize((width, height))
+            if user_image_actual:
+                st.image(user_image_actual)
+
+        with col2:
+            st.info("Enter masked image here")
+            user_image_masked = st.file_uploader("", key="file_up2")
+            # resizing the Image
+            width2, height2 = 256, 256
+            # image = user_image.resize((width, height))
+            if user_image_masked:
+                st.image(user_image_masked)
+        user_img_actual_formatted = None
+        user_img_masked_formatted = None
+        selection_image_size = ["256x256", "512x512", "1024x1024"]
+        selection_size = st.sidebar.radio("Select Size", selection_image_size)
+        # size selection menu
+        if selection_size == "256x256":
+            size = "256x256"
+            if user_image_actual:
+                image_stream_actual = BytesIO(user_image_actual.read())
+                user_img_actual_formatted = Image.open(image_stream_actual)
+                user_img_actual_formatted = user_img_actual_formatted.resize((256, 256))
+                user_img_actual_formatted = user_img_actual_formatted.convert("RGB")
+                st.image(user_img_actual_formatted)
+                with BytesIO() as buffer:
+                    user_img_actual_formatted.save(buffer, format="JPEG")
+                    actual_img_bytes = buffer.getvalue()
+            if user_image_masked:
+                image_stream_mask = BytesIO(user_image_masked.read())
+                user_img_masked_formatted = Image.open(image_stream_mask)
+                user_img_masked_formatted = user_img_masked_formatted.resize((256, 256))
+                user_img_masked_formatted = user_img_masked_formatted.convert("RGB")
+                st.image(user_img_masked_formatted)
+                with BytesIO() as buffer:
+                    user_img_masked_formatted.save(buffer, format="JPEG")
+                    masked_img_bytes = buffer.getvalue()
+        elif selection_size == "512x512":
+            size = "512x512"
+            if user_image_actual:
+                image_stream_actual = BytesIO(user_image_actual.read())
+                user_img_actual_formatted = Image.open(image_stream_actual)
+                user_img_actual_formatted = user_img_actual_formatted.resize((512, 512))
+                user_img_actual_formatted = user_img_actual_formatted.convert("RGB")
+                st.image(user_img_actual_formatted)
+                with BytesIO() as buffer:
+                    user_img_actual_formatted.save(buffer, format="JPEG")
+                    actual_img_bytes = buffer.getvalue()
+            if user_image_masked:
+                image_stream_mask = BytesIO(user_image_masked.read())
+                user_img_masked_formatted = Image.open(image_stream_mask)
+                user_img_masked_formatted = user_img_masked_formatted.resize((512, 512))
+                user_img_masked_formatted = user_img_masked_formatted.convert("RGB")
+                st.image(user_img_masked_formatted)
+                with BytesIO() as buffer:
+                    user_img_masked_formatted.save(buffer, format="JPEG")
+                    masked_img_bytes = buffer.getvalue()
+        elif selection_size == "1024x1024":
+            size = "1024x1024"
+            if user_image_actual:
+                image_stream_actual = BytesIO(user_image_actual.read())
+                user_img_actual_formatted = Image.open(image_stream_actual)
+                user_img_actual_formatted = user_img_actual_formatted.resize((1024, 1024))
+                user_img_actual_formatted = user_img_actual_formatted.convert("RGB")
+                st.image(user_img_actual_formatted)
+                with BytesIO() as buffer:
+                    user_img_actual_formatted.save(buffer, format="JPEG")
+                    actual_img_bytes = buffer.getvalue()
+            if user_image_masked:
+                image_stream_masked = BytesIO(user_image_actual.read())
+                user_img_masked_formatted = Image.open(image_stream_masked)
+                user_img_masked_formatted = user_img_masked_formatted.resize((1024, 1024))
+                user_img_masked_formatted = user_img_masked_formatted.convert("RGB")
+                st.image(user_img_masked_formatted)
+                # with open(user_img_masked_formatted, "rb") as img:
+                with BytesIO() as buffer:
+                    user_img_masked_formatted.save(buffer, format="JPEG")
+                    masked_img_bytes = buffer.getvalue()
+        no_of_images = st.sidebar.number_input("How many image variations you want", min_value=1, max_value=10, value=1)
+        user_input = functions.text_area_img()
+        image_bot = functions.Chatbot()
+        Generate_button = st.sidebar.button("Generate Response")
+        if Generate_button:
+            if user_input == "":
+                st.warning("Please Enter some Prompt")
+            elif user_img_masked_formatted is None or user_img_actual_formatted is None:
+                st.warning("Browse for Image files first")
+            else:
+                # with open(user_img_masked_formatted, "rb") as img_act:
+                #     actual_img_bytes = img_act.read
+                image_bot_response = image_bot.edit_image(actual_img_bytes, masked_img_bytes, user_input, size, no_of_images)
+                for j, image_response_link in enumerate(image_bot_response):
+                    img_name = f"{date}"
+
+                    def save_image():
+                        st.info("Saving Image to disk")
+                        save_path = f"{directory_name}\\{img_name}-{i}.png"
+                        functions.download_image(image_response_link, save_path)
+
+                    save_image()
+                    i += 1
+                # image_bot_response = image_bot.get_image(user_input, size, no_of_images=1)
+                # for image in image_bot_response:
+                #     st.image(image_bot_response)
+                #     st.write(image_bot_response)
+                st.success("Generating Response")
+
+    # Make variation of images
+    elif selection == selection_image_type[2]:
+        st.header("Make Varaitions of Images")
+        user_image = st.file_uploader("Enter a Valid image file ")
+        if user_image:
+            # resizing the Image
+            width, height = 256, 256
+            image = user_image.resize((width, height))
+            st.image(user_image)
+        selection_image_size = ["256x256", "512x512", "1024x1024"]
+        selection_size = st.radio("Select Size", selection_image_size)
+        # setting size
+        if selection_size == selection_image_size[0]:
+            size = "256x256"
+        elif selection_size == selection_image_size[1]:
+            size = "512x512"
+        elif selection_size == selection_image_size[2]:
+            size = "1024x1024"
+
+
+
+
+if selected_option == "History":
+    selection_image_type = ["Image Creation History", "Image Editing History", "Variations History"]
+    selection = st.sidebar.selectbox("Selection Menu", selection_image_type)
+    if selection == "Image Creation History":
+        # saving directory path
+        directory_path = f"files\\images\\Creation"
+        # finding all folders in directory
+        history_dir = os.listdir(directory_path)
+        history_dir_names = [dir for dir in history_dir]
+        # making a radio button based on directory names which means history days
+        selected_day = st.sidebar.radio("Select Day", history_dir_names)
+        # checking for every day which one is selected
+        for history_dir_name in history_dir_names:
+            if selected_day == history_dir_name:
+                st.header("Image Creation History")
+                st.info(f"Date : {history_dir_name}")
+                # based on selected day diplaying images
+                images = (glob.glob(f"{directory_path}\\{history_dir_name}/*.png"))
+                if len(images) == 0:
+                    st.warning("Nothing to show here")
+                else:
+                    st.image(images)
+
